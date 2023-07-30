bb3ece6771715c61642a6d8c65979a10ea46382f
Denperidge <denperidge@gmail.com>
2023-07-30 20:42:51 +0200

    Reworked utils/files.py and added tests

--- src/divio_docs_parser/utils/files.py
+++ src/divio_docs_parser/utils/files.py
@@ -1,32 +1,16 @@
 # Built-in imports
-from os import makedirs
-from os.path import join, exists
+from os.path import join, abspath, isfile
 from glob import glob
 
-"""Utilities for reading files"""
+"""Utilities for finding files"""
 
-def get_file(self, path) -> str:
-    """(Method) Returns the path to a file, automatically prefixing the repos dir path if needed"""
-    if path.startswith(self.local_dir):
-        return path
-    return join(self.local_dir, path)
+def list_all_files(path: str, selector="*"):
+    """(Method) Run a glob on the specified path within the provided path"""
+    path = abspath(path)
+    files = glob(join(path, "**/" + selector), recursive=True)
+    return [file for file in files if isfile(file)]
 
-def find_files(self, path):
-    """(Method) Run a glob on the specified path within this repo"""
-    return glob(self.get_file(path), recursive=True)
-
-def _all_markdown_files(self):
+def list_all_markdown_files(path):
     """(Property) Returns a list of all markdown files of the repo"""
-    return self.find_files("**/*.md")
-all_markdown_files = property(_all_markdown_files)
+    return list_all_files(path, "*.md")
 
-def get_file_contents(self, path):
-    """(Method) Reads contents from specified file"""
-    # If a relative path is given, append
-    path = self.get_file(path)
-    try:
-        with open(path, "r", encoding="UTF-8") as file:
-            data = file.read()
-    except FileNotFoundError:
-        raise FileNotFoundError
-    return data
