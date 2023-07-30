acdcf38bbf067d49506eb5cab636cf62fad56c23
Denperidge <denperidge@gmail.com>
2023-07-30 19:12:45 +0200

    Refactored md_to_sections & adapted tests

--- src/test_md_to_sections.py
+++ src/test_md_to_sections.py
@@ -1,6 +1,7 @@
 import unittest
 
-from divio_docs_parser.md_to_sections import parse_all_sections_from_markdown, _parse_all_sections_from_markdown_file, _parse_all_sections_from_markdown_string
+from divio_docs_parser.md_to_sections import parse_sections_from_markdown, _parse_sections_from_markdown_file, _parse_sections_from_markdown_string
+from divio_docs_parser.DivioDocs import DivioDocs
 
 
 expected_output = {
@@ -32,9 +33,15 @@ How to...
 test_data_path = "tests/test_data/README.md"
 
 class TestDivioDocs(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sections = DivioDocs()._sectionObjects
+    
+    def tearDown(self) -> None:
+        self.sections = None
+        
     
     def test_parse_all_sections_from_markdown_file(self):
-        data = _parse_all_sections_from_markdown_file(test_data_path)
+        data = _parse_sections_from_markdown_file(self.sections, test_data_path)
         self.assertDictEqual(data, expected_output)
         
        
@@ -42,7 +49,7 @@ class TestDivioDocs(unittest.TestCase):
         with open(test_data_path, "r", encoding="utf-8") as file:
             input = file.read()
 
-        data = _parse_all_sections_from_markdown_string(input)
+        data = _parse_sections_from_markdown_string(self.sections, input)
         self.assertDictEqual(data, expected_output)
 
     
@@ -52,7 +59,7 @@ class TestDivioDocs(unittest.TestCase):
 
         inputs = [test_data_path, test_data_string]
         for input in inputs:
-            data = parse_all_sections_from_markdown(input)
+            data = parse_sections_from_markdown(self.sections, input)
             self.assertDictEqual(data, expected_output)
 
 
